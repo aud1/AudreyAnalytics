99de6f3f907ba4854802059168ef1264cebb6818
Audrey <shoppingalways@comcast.net>
2017-06-02 10:59:41 -0700

    just a test

--- app/src/main/java/com/hfad/audreyanalytics/MainActivity.java
+++ app/src/main/java/com/hfad/audreyanalytics/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //just a TEST2
+        //just a TEST
         setContentView(R.layout.activity_main);
     }
 }
